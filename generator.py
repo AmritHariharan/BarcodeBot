4bad0e17b53a46db0f4f145bf0ca7dc43a036e46
Amrit Hariharan <amrit.harih@gmail.com>
2017-11-28 18:33:26 +0000

    started youtube_dl stuff

--- generator.py
+++ generator.py
@@ -1,11 +1,32 @@
 import cv2
+import youtube_dl
 #import numpy as np
+
+# Logger object for youtube_dl output
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+# not sure what this is but according to the documentation I need it...        
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
 
 class GenerateImage:
 
+	def download_link(self, link):
+			
+		
 	def generate(self, filename):
 		reader = cv2.VideoCapture(filename)
 		success, image = reader.read()
+		
 		count = 0
 		success = True;
 		while success:
