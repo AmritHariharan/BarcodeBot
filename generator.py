ff88e1689d6b60921a02e0ac423f68c42ac093c2
Amrit Hariharan <amrit.harih@gmail.com>
2017-11-29 02:18:19 +0000

    still trying to get processing to work

--- generator.py
+++ generator.py
@@ -1,27 +1,27 @@
 import cv2
-import youtube_dl
+#import youtube_dl
 #import numpy as np
 
 # Logger object for youtube_dl output
-class MyLogger(object):
-    def debug(self, msg):
-        pass
-
-    def warning(self, msg):
-        pass
-
-    def error(self, msg):
-        print(msg)
-
+#class MyLogger(object):
+#    def debug(self, msg):
+#        pass
+#
+#    def warning(self, msg):
+#        pass
+#
+#    def error(self, msg):
+#        print(msg)
+#
 # not sure what this is but according to the documentation I need it...        
-def my_hook(d):
-    if d['status'] == 'finished':
-        print('Done downloading, now converting ...')
+#def my_hook(d):
+#    if d['status'] == 'finished':
+#        print('Done downloading, now converting ...')
 
 class GenerateImage:
 
 	def download_link(self, link):
-			
+		print("test")	
 		
 	def generate(self, filename):
 		reader = cv2.VideoCapture(filename)
@@ -35,4 +35,10 @@ class GenerateImage:
 			cv2.imwrite("frame%d.png" % count, image)
 			count += 1
 			average_color = [img[:, :, i].mean() for i in range(img.shape[-1])] # does this work???
+			
+
+if __name__ == "__main__":
+	var = GenerateImage()
+	
+	var.generate(stay.mp4)
 			
