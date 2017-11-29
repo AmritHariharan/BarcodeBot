7120afb17ae00f544e538fac4b1b098829db35a6
Amrit Hariharan <amrit.harih@gmail.com>
2017-11-29 02:43:50 +0000

    fixed iterating logic, now for colourthief

--- generator.py
+++ generator.py
@@ -1,7 +1,7 @@
 #import os
 import cv2
 from sklearn.cluster import KMeans
-from math import ceil
+from math import floor
 #from colorthief import ColorThief
 #import numpy as np
 
@@ -19,21 +19,20 @@ class GenerateImage:
 		px_count = 0
 		im_count = 1
 		#print("1: %s    2: %s" % (image.shape[0], image.shape[1]))
-		w = image.shape[1]
-		q_length = ceil(w / width) # NOTE: THIS CANNOT BE 0
-		print("q length: %s" % q_length)
+		length = int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
+		q_length = floor(length / width) # NOTE: THIS CANNOT BE 0
+		print("1: %d    2: %d" % (length, q_length))
+		#print("q length: %s" % q_length)
 
 
 		while success:
 			success, image = reader.read()
-			print('Read a new frame: ', success)
+			#print('Read a new frame: ', success)
 			if im_count % q_length == 0:
-				print('Writing an image')
-				cv2.imwrite("%s-frame%d.png" % (filename, im_count), image)
+				print('Writing image %d' % px_count)
+				cv2.imwrite("%s-frame%d.png" % (filename, px_count), image)
+				px_count += 1
 			im_count += 1
-
-			# Average colour
-			#average_color = [img[:, :, i].mean() for i in range(img.shape[-1])] # does this work???
 
 
 if __name__ == "__main__":
