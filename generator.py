59805f862a38e67e3227d7524904e909e09a3531
Amrit Hariharan <amrit.harih@gmail.com>
2017-11-29 02:33:55 +0000

    getting to generate images, modulo isn't working as it should

--- generator.py
+++ generator.py
@@ -1,44 +1,41 @@
+#import os
 import cv2
-#import youtube_dl
+from sklearn.cluster import KMeans
+from math import ceil
+#from colorthief import ColorThief
 #import numpy as np
 
-# Logger object for youtube_dl output
-#class MyLogger(object):
-#    def debug(self, msg):
-#        pass
-#
-#    def warning(self, msg):
-#        pass
-#
-#    def error(self, msg):
-#        print(msg)
-#
-# not sure what this is but according to the documentation I need it...        
-#def my_hook(d):
-#    if d['status'] == 'finished':
-#        print('Done downloading, now converting ...')
+#os.system("cd ..")
 
 class GenerateImage:
 
-	def download_link(self, link):
-		print("test")	
-		
 	def generate(self, filename):
 		reader = cv2.VideoCapture(filename)
 		success, image = reader.read()
-		
-		count = 0
 		success = True;
+		#n_colors = 1
+		width = 720
+		height = 480
+		px_count = 0
+		im_count = 1
+		#print("1: %s    2: %s" % (image.shape[0], image.shape[1]))
+		w = image.shape[1]
+		q_length = ceil(w / width) # NOTE: THIS CANNOT BE 0
+		print("q length: %s" % q_length)
+
+
 		while success:
 			success, image = reader.read()
 			print('Read a new frame: ', success)
-			cv2.imwrite("frame%d.png" % count, image)
-			count += 1
-			average_color = [img[:, :, i].mean() for i in range(img.shape[-1])] # does this work???
-			
+			if im_count % q_length == 0:
+				print('Writing an image')
+				cv2.imwrite("%s-frame%d.png" % (filename, im_count), image)
+			im_count += 1
+
+			# Average colour
+			#average_color = [img[:, :, i].mean() for i in range(img.shape[-1])] # does this work???
+
 
 if __name__ == "__main__":
 	var = GenerateImage()
-	
-	var.generate(stay.mp4)
-			
+	var.generate("stay.mp4")
