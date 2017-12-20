3d1ab7101db3a10c7664c163ae54522169d01819
AmritHariharan <stuka241@gmail.com>
2017-12-19 22:18:14 -0500

    outputs image, rgb is staying separated though

--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import cv2
+import numpy as np
 from math import floor
 
 class GenerateImage:
@@ -8,20 +9,20 @@ class GenerateImage:
         reader = cv2.VideoCapture(filename)
         success, image = reader.read()
         success = True;
-        width = 720
-        height = 480
+        width = 640
+        height = 360
         px_count = 0
         im_count = 1
         #print("1: %s    2: %s" % (image.shape[0], image.shape[1]))
         length = int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
         q_length = floor(length / width) # NOTE: THIS CANNOT BE 0
 
-        print("pls")
-
         # Check that its not longer than 10 mins
         if length > 14400:
             print("Sorry, this video is too long, try again with a video under 10 minutes in length")
             quit() # TODO: find a more elegant way to do this
+
+        colours = []
 
         while success:
             # 1. read in image
@@ -29,24 +30,33 @@ class GenerateImage:
             if im_count % q_length == 0:
                 #print('Processing image %d' % px_count)
                 pil_im = Image.fromarray(image) # get PIL image
-                pil_im.show()
+                
                 # 2. get enumerated list of colours
-                colours = pil_im.getcolors()
+                im_colours = pil_im.getcolors(width * height)
+                
                 # 3. get most dominant colour
-                most_freq = colours[0]
-                for count, colour in colours:
-                    if count > most_freq(0):
+                most_freq = im_colours[0]
+                for count, colour in im_colours:
+                    if count > most_freq[0]:
                         most_freq = (count, colour)
+                
                 # 4. write to column of new image
-                # TODO: this...
+                colours.append(colour)
                 # iterate
                 px_count += 1
             im_count += 1
+        im_count -= 1
 
         # 5. write new image
-
+        im_arr = np.array(colours)
+        print(len(colours))
+        print(len(im_arr))
+        im_arr = np.resize(im_arr, (480,len(im_arr))) 
+        final = Image.fromarray(im_arr, 'RGB')
+        final.save('output.png')
+        final.show()
 
 if __name__ == "__main__":
 	var = GenerateImage()
-	print("starting")
-	var.generate("stay.mp4")
+	#var.generate("stay.mp4")
+	var.generate("zodiac.mp4")
