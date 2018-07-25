e651daae7833c4f4ea57a1867acc1bd8ca5b2dcd
Amrit Hariharan <amrit.harih@gmail.com>
2018-07-24 22:15:05 -0400

    Finished refactoring for now

--- generator.py
+++ generator.py
@@ -1,7 +1,6 @@
 # Image/video processing stuff
 from PIL import Image
 import cv2
-import numpy as np
 from math import floor
 
 # YouTube
@@ -11,86 +10,81 @@ from validators import url
 from sys import stdout
 
 
+def check_video_dimensions(frame_count, width):
+    # Check that its not longer than 10 mins
+    if frame_count > 54000:
+        print("Sorry, this video is too long, try again with a video under 30 minutes in length")
+        quit()  # TODO: find a more elegant way to do this
+
+    # Check that its not too short
+    if frame_count < width:
+        print('Sorry, this video is too short (or possibly too high res)')
+        quit()
+
+
+def write_column(column, width, height, image, final_image):
+    for row in range(height):
+        # 1. get enumerated list of the rows colours
+        im_colours = Image.fromarray(image[row].reshape(1, width, 3)).getcolors(width)
+
+        # 2. get most dominant colour in row
+        most_freq = im_colours[0]
+        for count, colour in im_colours:
+            if count > most_freq[0]:
+                most_freq = (count, colour)
+
+        # 3. write pixel to final image
+        final_image[row][column - 1] = most_freq[1]
+
+
 class Generator:
 
-    # Get the barcode from a video
-    def generate_barcode(self, filename, output_filename):
+    def generate_barcode(self, filename, output_filename, debug=False):
         print('Generating a barcode')
 
         # Setup
         reader = cv2.VideoCapture(filename)
-        success, image = reader.read()
+        success, frame = reader.read()
         success = True
-        height = len(image)
-        width = len(image[0])
-        im_count = 1
-        out = image
+        frame_height = len(frame)
+        frame_width = len(frame[0])
+        result_array = frame
 
-        frame_count = int(reader.get(cv2.CAP_PROP_FRAME_COUNT))
-        frame_count = frame_count / 2  # TODO: Find a better fix than this...
-        q_length = floor(frame_count / width)  # NOTE: THIS CANNOT BE 0
+        frame_count = int(reader.get(cv2.CAP_PROP_FRAME_COUNT)) / 2
+        queue_length = floor(frame_count / frame_width)  # NOTE: THIS CANNOT BE 0
 
-        self.check_video_dimensions(frame_count, width)
+        check_video_dimensions(frame_count, frame_width)
 
-        counter = 0
-
+        queue_counter = 0
+        num_processed_images = 0
         while success:
+            queue_counter += 1
             # 1. read in image
-            success, image = reader.read()
-            if im_count == q_length:
-                im_count = 0
-                counter += 1
-                print('\rProgress: %d/%d' % (counter, width), end='\r')
+            success, frame = reader.read()
+            if queue_counter == queue_length:
+                queue_counter = 0
+                num_processed_images += 1
+                print('\rProgress: %d/%d' % (num_processed_images, frame_width), end='\r')
                 stdout.flush()
-                self.write_column(counter, width, height, image, out)
+                write_column(num_processed_images, frame_width, frame_height, frame, result_array)
 
             # break if you reach the end of the image array
-            if counter == width:
+            if num_processed_images == frame_width:
                 print('Finished processing image.')
                 break
-            im_count += 1
 
-        # debug print statements
-        print('length: {}'.format(frame_count))
-        print('q_length: {}'.format(q_length))
-        print('im_count: {}'.format(im_count))
-        print('counter: {}'.format(counter))
-        print('width: {}'.format(width))
+        final_image = Image.fromarray(result_array, 'RGB')
 
-        # 5. write new image
-        final = Image.fromarray(out, 'RGB')
-        final.save(output_filename)
+        if debug:
+            print('length: {}'.format(frame_count))
+            print('q_length: {}'.format(queue_length))
+            print('counter: {}'.format(num_processed_images))
+            print('width: {}'.format(frame_width))
+            final_image.save(output_filename)
 
-    def write_column(self, column, width, height, image, final_image):
-        for row in range(height):
-            # 1. get row of image
-            pil_im = Image.fromarray(image[row].reshape(1, width, 3))
-
-            # 2. get enumerated list of colours
-            im_colours = pil_im.getcolors(width)
-
-            # 3. get most dominant colour in row
-            most_freq = im_colours[0]
-            for count, colour in im_colours:
-                if count > most_freq[0]:
-                    most_freq = (count, colour)
-
-            # 4. write to of new image
-            final_image[row][column - 1][0] = most_freq[1][0]
-            final_image[row][column - 1][1] = most_freq[1][1]
-            final_image[row][column - 1][2] = most_freq[1][2]
-
-    def check_video_dimensions(self, frame_count, width):
-        # Check that its not longer than 10 mins
-        if frame_count > 54000:
-            print("Sorry, this video is too long, try again with a video under 30 minutes in length")
-            quit()  # TODO: find a more elegant way to do this
-        # Check that its not too short
-        if frame_count < width:
-            print('Sorry, this video is too short (or possibly too high res)')
-            quit()
+        return final_image
 
 
 if __name__ == '__main__':
     generator = Generator()
-    generator.generate_barcode('firestone.mp4', 'firestone.png')
+    generator.generate_barcode('firestone.mp4', 'firestone.png', debug=True)
