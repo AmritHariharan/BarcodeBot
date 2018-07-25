70681f790b7bf2b3dd1eecd523b6dc3ed3c8f395
Amrit Hariharan <amrit.harih@gmail.com>
2018-07-25 00:02:41 -0400

    Basic file uploading to uploads directory done

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from sys import stderr
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
-from os import listdir
+from os import listdir, path
 
-from validators import url
+from werkzeug.utils import secure_filename
 
 STATIC_IMAGES_DIR = 'static/images'
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'mp4', 'mov', 'mp3'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 Bootstrap(app)
 
 
@@ -27,5 +30,30 @@ def generate_barcode():
     return redirect(url_for('start'))
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has a file
+        if 'file' not in request.files:
+            flash('No file in request')
+            return redirect(request.url)
+        file = request.files['file']
+        print(file)
+        # if user does not select file, browser also submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('start'))
+
+
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    # app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True)
