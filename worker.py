109d3e031e1bb692030dd1de96da8913da57ef17
Amrit Hariharan <amrit.harih@gmail.com>
2018-08-04 10:57:31 -0400

    added rq worker file

---
+++ worker.py
@@ -0,0 +1,14 @@
+from os import getenv
+from redis import from_url
+from rq import Worker, Queue, Connection
+
+listen = ['high', 'default', 'low']
+
+redis_url = getenv('REDISTOGO_URL', 'redis://localhost:6379')
+
+conn = from_url(redis_url)
+
+if __name__ == '__main__':
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
